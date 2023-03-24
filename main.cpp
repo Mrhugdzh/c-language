d99e329302948dfc08d473b82bb2fac0c1fda61f
hugang <2777576216@qq.com>
2023-03-24 16:37:50 +0800

    SkipList add search , dump_file, load_file

--- main.cpp
+++ main.cpp
@@ -4,15 +4,37 @@
 using namespace std;
 
 int main() {
-    SkipList<int, string> sp(10);
-    sp.insert_value(0, "I");
-    sp.insert_value(1, "am");
-    sp.insert_value(2, "hugang");
-    sp.insert_value(3, "now");
-    sp.insert_value(4, "learning");
-    sp.insert_value(5, "skipList");
+    SkipList<string, string> sp(10);
+    // sp.insert_value(0, "I");
+    // sp.insert_value(1, "am");
+    // sp.insert_value(2, "hugang");
+    // sp.insert_value(3, "now");
+    // sp.insert_value(4, "learning");
+    // sp.insert_value(5, "skipList");
 
+    // sp.display();
+
+    // sp.delete_value(0);
+
+    // sp.display();
+
+    // sp.delete_value(3);
+
+    // sp.display();
+
+    // cout<<sp.search(5)<<endl;
+    // sp.search(0);
+
+    // sp.set_value(2, "Liming");
+    // sp.display();
+
+    // sp.dump_file();
+
+    sp.load_file();
     sp.display();
+
+
+    
 
     
 
--- skiplist.h
+++ skiplist.h
@@ -2,10 +2,12 @@
 #include <cstdlib>
 #include <cstring>
 #include <mutex>
+#include <fstream>
 
 using namespace std;
 
 mutex mtx;
+string store_file="./store/dumpFile";
 
 template<typename K, typename V>
 class Node{
@@ -72,22 +74,29 @@ class SkipList{
         /*定义增删改查*/
         bool insert_value(K k, V v);
         bool delete_value(K v);
-        bool set_value(K k);
+        bool set_value(K k, V v);
         void display();
-        bool search(K k);
+        V search(K k);
+
+        void dump_file();
+        void load_file();
 
         int size();
         Node<K, V>* create_node(K, V, int);
 
     private:
         int get_random_level();
-
+        void get_key_value_from_line(string line, string *key, string *value);
+        bool is_valid(const string str);
 
     private:
         int _max_level; // 跳表最大的�?
         int _current_level; // 跳表当前的层�?
         Node<K, V>* _header; // 头结�?
         int _node_num; // 当前跳表的节点个�?
+
+        std::ofstream _file_writer;
+        std::ifstream _file_reader;
 };
 
 template<typename K, typename V>
@@ -113,6 +122,14 @@ SkipList<K, V>::SkipList(int max_level):_max_level(max_level){
 
 template<typename K, typename V>
 SkipList<K, V>::~SkipList(){
+    if(this->_file_writer.is_open()) {
+        this->_file_writer.close();
+    }
+
+    if(this->_file_reader.is_open()) {
+        this->_file_reader.close();
+    }
+
     delete _header;
 }
 
@@ -134,7 +151,6 @@ int SkipList<K, V>::get_random_level(){
 template<typename K, typename V>
 void SkipList<K, V>::display(){
     cout<<"\n**********Skip List**********\n";
-    cout<<this->_current_level<<endl;
     for(int i=0;i<=this->_current_level;i++){
         /*获取这一行的头部节点*/
         Node<K, V>* node = this->_header->forward[i];
@@ -198,20 +214,128 @@ template<typename K, typename V>
 bool SkipList<K, V>::delete_value(K k) {
     mtx.lock();
     Node<K, V>* current = this->_header;
+    Node<K,V>* update[this->_max_level+1];
+    memset(update, 0, sizeof(Node<K, V>*) * (this->_max_level+1));
     for(int i=this->_current_level;i>=0;i--){
         while(current->forward[i]!=nullptr && current->forward[i]->get_key()<k) {
+            
             current = current->forward[i];
         }
 
-        if(current->forward[i]->get_key()==k) {
-            // 可以删除
-            current->forward[i] = current->forward[i]->forward[i];
-            cout<<"delete successful\n";
-            mtx.unlock();
-            return 1;
+        update[i]=current;
+    }
+
+    current=current->forward[0]; // 找到待删除的节点
+    /*current==nullptr 没有这个节点*/
+    /*current->get_key()!=k 没有这个节点*/
+    if(current!=nullptr || current->get_key()==k) {
+        //需要进行删除
+        for(int i=0; i<=this->_current_level; i++) {
+            if(update[i]->forward[i]!=current) { // 当前层没有，那么上面的所有层都没有
+                break;
+            }
+            update[i]->forward[i]=current->forward[i];
         }
+
+        while(_current_level>0 && this->_header->forward[_current_level]==0) {
+            --_current_level;
+        }
+
+        --_node_num;
     }
     mtx.unlock();
     return 0;
 }
 
+template<typename K, typename V>
+V SkipList<K, V>::search(K k) {
+    V result;
+    Node<K, V>* current = this->_header;
+    for(int i=this->_current_level; i>=0; i--) {
+        while(current->forward[i]!=nullptr && current->forward[i]->get_key()<k){
+            current = current->forward[i];
+        }
+    }
+
+    current = current->forward[0];
+    if(current->get_key()==k) {
+        result = current->get_value();
+    } else {
+        cout<<"There is no key "<<k<<endl;
+    }
+    return result;
+}
+
+template<typename K, typename V>
+bool SkipList<K, V>::set_value(K k, V v) {
+    Node<K, V>* current = this->_header;
+    for(int i=this->_current_level; i>=0; i--) {
+        while(current->forward[i]!=nullptr && current->forward[i]->get_key()<k){
+            current = current->forward[i];
+        }
+    }
+
+    current = current->forward[0];
+    if(current->get_key()==k) {
+        current->set_value(v);
+        cout<<"set key "<<k<<" to value "<<v<<endl;
+        return 1;
+    } else {
+        cout<<"There is no key "<<k<<endl;
+    }
+    return 0;
+}
+
+template<typename K, typename V>
+bool SkipList<K, V>::is_valid(const string str) {
+    if(str.size()==0) {
+        return 0;
+    }
+    if(str.find(":") == string::npos) {
+        return false;
+    }
+    return true;
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::get_key_value_from_line(string line, string *key, string *value) {
+    if(!is_valid(line)) {
+        cout<<"line is valid\n";
+        return ;
+    }
+
+    *key = line.substr(0, line.find(":"));
+    cout<<key<<endl;
+    *value = line.substr(line.find(":"), line.size()-1);
+}
+
+
+template<typename K, typename V>
+void SkipList<K, V>::dump_file(){
+    _file_writer.open(store_file);
+    Node<K, V>* current = this->_header->forward[0];
+
+    while(current!=nullptr) {
+        _file_writer<<current->get_key()<<":"<<current->get_value()<<"\n";
+        current=current->forward[0];
+    }
+    _file_writer.flush();
+    _file_writer.close();
+}
+
+template<typename K, typename V>
+void SkipList<K, V>::load_file() {
+    _file_reader.open(store_file);
+    string line;
+    string *key= new std::string();
+    string *value= new std::string();
+    while(getline(_file_reader, line)){
+        get_key_value_from_line(line, key, value);
+        if(key->empty() || value->empty()) {
+            continue;
+            
+        }
+        this->insert_value(*key, *value);
+    }
+    _file_reader.close();
+}
